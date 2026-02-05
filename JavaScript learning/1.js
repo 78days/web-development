c080f556f6c91f02e48a40857c33d1c3d19fa8fd
Ali faisal <alifaisal2005@outlook.com>
2026-02-05 22:44:12 +0500

    day 84 progess

---
+++ JavaScript learning/1.js
@@ -0,0 +1,8 @@
+// Messy JS code
+function   messy(x,y){if(x>y){console.log( "x is bigger") }else{console.log('y is bigger') } } 
+const   arr=[3,1,2, 5,4];arr.sort(function(a,b){return b-a}); console.log(arr)
+let obj={name :'Ali', age:23,city:"Karachi"}; console.log( obj.name,obj.age , obj.city)
+for(let i=0;i<5;i++){console.log(i)}
+const   add=(a,b)=>{return a+b} ;console.log(add(5,7))
+let unusedVar=42
+function messy2(){console.log("this is messy too")}; messy2()
---
+++ JavaScript learning/README.txt
@@ -0,0 +1,2 @@
+intent to follow javascript roadmap on roadmap.sh and solve javascript interview questions
+https://github.com/sudheerj/javascript-interview-questions
